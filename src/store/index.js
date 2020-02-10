b27eba7108efb654318e6526c65e613f88f33ed2
yameste <yameste@yahoo.co.uk>
2020-02-10 12:52:24 +0000

    removed references to data and object and replaced with store js with references to the state in the update methods

---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+
+export default new Vuex.Store({
+
+    state: {
+        products: []
+    },
+
+    getters: {
+        // computed properties
+        productsCount () {
+
+        }
+    },
+
+    actions: {
+        fetchProducts () {
+            // make the call
+            // run setProducts mutation
+
+        }
+    },
+
+    mutations: {
+        setProducts (state, products) {
+            // update products
+            state.products = products
+
+        }
+    }
+})
